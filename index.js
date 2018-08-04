51e560551a15f9f6a8e8a1af36f7bef1150745ca
Vishal Sharma <vsh1819@gmail.com>
2018-08-04 07:20:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function takeANumber(peopleArray, name){
   var number
-  
+  for (var i = 0; i < peopleArray.length; i++) {
+    
+  }
   return `Welcome, Ada. You are number ${peopleArray[]} in line.`
 }
