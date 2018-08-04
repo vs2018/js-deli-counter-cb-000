920c686e4966d7d767ed28e937abf59f5fbd9d40
Vishal Sharma <vsh1819@gmail.com>
2018-08-04 07:39:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine(line) {
   if (line.length === 0 ){
     return "The line is currently empty."
   }
-  var string
+  var string = ""
   for(var i = 0; i < line.length; i++) {
     
   }
