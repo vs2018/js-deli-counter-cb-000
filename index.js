9e3afac8bf27980362eb19a8fbc577e7098ff09d
Vishal Sharma <vsh1819@gmail.com>
2018-08-04 07:24:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 function takeANumber(peopleArray, name){
-  var number
-  for (var i = 0; i < peopleArray.length; i++) {
-    if (name === peopleArray[i]) {
-      number = i + 1
-    }
-  }
   return `Welcome, ${name}. You are number ${number} in line.`
 }
